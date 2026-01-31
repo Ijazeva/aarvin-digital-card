67f1e7e26e447c357dfab09813260d4f30ace83f
Ijazeva <ijazeva69@gmail.com>
2026-01-31 11:46:02 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "aarvin-clean-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
